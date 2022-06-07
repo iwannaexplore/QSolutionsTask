6dc2f6dd5315ebbf1c696672ef5e18c77d50adc6
Dmitry <iwannaexplore@gmail.com>
2022-06-07 11:25:58 +0300

    Added web reference (#1)
    * added web reference
    
    * Added web reference
    
    Co-authored-by: dmitriysenko <dmitriy.senko.job@gmai.com>

--- SoapToJson/AppConstants.cs
+++ SoapToJson/AppConstants.cs
@@ -2,7 +2,7 @@ namespace SoapToJson;
 
 public static class AppConstants
 {
-    public static string ApiUrl = "https://qws1.de/QWS/QACWebService.asmx?op=UCheckAddress";
+    public static string ApiUrl = "https://qws1.de/QWS/QACWebService.asmx";
     public static string TestUserName = "testuser";
     public static string TestUserPassword = "TgNmUh-uw!sl$";
     public static int TestTolerance = 2;
